975d12b33bcb932b1ec44e3101fc9a166b1f8303
devazarado <devazarado@gmail.com>
2023-10-20 12:48:43 -0300

    Adicionando as migrations no projeto

--- obj/Debug/net7.0/DEVAZARADO.AssemblyInfo.cs
+++ obj/Debug/net7.0/DEVAZARADO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DEVAZARADO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0abaf5a51cb42cde8d05f2689ffd1c94b5cc45f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0f8499a84ecf334a8c411307dbd2a85e31f4f6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DEVAZARADO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DEVAZARADO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
